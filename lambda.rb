79bd9a0d77ef4bab045e755a305a102391953f67
Jam <jam@jamandbees.net>
2013-01-21 19:44:33 -0700

    Parameters passed into procs have a different scope than external variables.

--- lambda.rb
+++ lambda.rb
@@ -1,3 +1,5 @@
+x = 1
+
 foo = Proc.new do |x|
   puts "In a proc. X is valued at #{x}"
 end
@@ -8,3 +10,5 @@ foo.call(24)
 bar = lambda { |x| puts "In a lambda. X is valued at #{x}"}
 
 bar.call(32)
+
+puts "#{x}"
