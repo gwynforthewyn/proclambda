21c71a588ffcca495a51072c8dac7df4b91a6cbe
Jam <jam@jamandbees.net>
2013-01-21 19:46:37 -0700

    Parameters not passed into procs are from the external scope. The namespace is the same.

--- lambda.rb
+++ lambda.rb
@@ -2,13 +2,15 @@ x = 1
 
 foo = Proc.new do
   puts "In a proc. X is valued at #{x}"
+  x = 25
 end
 
 foo.call
-
 
 bar = lambda { |x| puts "In a lambda. X is valued at #{x}"}
 
 bar.call(32)
 
+
 puts "#{x}"
+
