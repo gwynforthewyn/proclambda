34d15fa55e183ed93d6a14e704d9d00781761757
Jam <jam@jamandbees.net>
2013-01-21 19:48:52 -0700

    Defined a runnable method which yields to a block. Notice that the block is equivalent to an anonymous proc/lambda.

--- lambda.rb
+++ lambda.rb
@@ -8,9 +8,18 @@ end
 foo.call
 
 bar = lambda { |x| puts "In a lambda. X is valued at #{x}"}
-
 bar.call(32)
 
 
 puts "#{x}"
 
+
+def runnable
+  puts "Inside runnable."
+  yield
+  puts "Still inside runnable."
+end
+
+runnable do
+  puts "I am a block."
+end
